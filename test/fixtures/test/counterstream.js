33922d35c4dd2da63d8f41129297ec418258904f
micahelbag <bagishov.mikail@ya.ru>
2016-02-15 18:12:31 +0300

    fixed requires

--- test/fixtures/test/counterstream.js
+++ test/fixtures/test/counterstream.js
@@ -1,6 +1,6 @@
 "use strict";
 const assert = require("assert");
-const CounterStream = require("../CounterStream");
+const CounterStream = require("../counterstream");
 let stream = new CounterStream({max: 10});
 let calls = 0;
 stream
