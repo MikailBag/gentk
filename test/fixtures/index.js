1003c05dfdc8169f6f3fb38a4e35e89c3a7b1e77
micahelbag <bagishov.mikail@ya.ru>
2016-02-15 20:01:47 +0300

    added new tests

---
+++ test/fixtures/index.js
@@ -0,0 +1,1 @@
+exports.CounterStream = require("./counterstream");
--- test/test.js
+++ test/test.js
@@ -1,5 +1,24 @@
 "use strict";
 //here will be tests
-
-
-describe("maketk", function(){});
+const assert = require("assert");
+const maketk = require("../index");
+const fixtures = require("./fixtures");
+describe("maketk", function () {
+    describe("streaming", function () {
+        describe("pipeToGen", function () {
+            it("pipes stream to generator", function(callback){
+                let stream = new fixtures.CounterStream({max:10});
+                let calls = 0;
+                function* gen(stream){
+                    while (yield){
+                        calls++
+                    }
+                }
+                stream.on('end', function(){
+                    assert.equal(calls, 10);
+                    callback()
+                })
+            })
+        })
+    })
+});
