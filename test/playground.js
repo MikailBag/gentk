42ecccb7496e1f6c9292928306163b7686c96cd6
micahelbag <bagishov.mikail@ya.ru>
2016-02-15 18:08:27 +0300

    fixed requires

--- test/playground.js
+++ test/playground.js
@@ -1,6 +1,6 @@
 "use strict";
 const assert = require("assert");
-const CounterStream = require("./fixtures/CounterStream");
+const CounterStream = require("./fixtures/counterstream");
 let stream = new CounterStream({max: 10});
 let calls = 0;
 stream
