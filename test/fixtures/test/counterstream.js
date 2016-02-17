0a171690d4b7980bd82774768cd6c3705dbc0c5c
MikailBag <bagishov444.2002.02@gmail.com>
2016-02-17 13:28:28 +0300

    add debug output

--- test/fixtures/test/counterstream.js
+++ test/fixtures/test/counterstream.js
@@ -4,8 +4,9 @@ const CounterStream = require("../counterstream");
 let stream = new CounterStream({max: 10});
 let calls = 0;
 stream
-    .on("data", function () {
+    .on("data", function (data) {
         calls++;
+        console.log(`!!!!!!got data chunk#${calls}:${data}`);
     })
     .on('end', function () {
         assert.equal(calls, 10);
